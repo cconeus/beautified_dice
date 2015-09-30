931ed5ebc632b7ab2fbbfcf2b174ed8c3d35f55b
Dana Sharman <student@epicodus.com>
2015-09-30 16:14:05 -0700

    removed more junk

--- dice/dice.js
+++ dice/dice.js
@@ -98,10 +98,7 @@
         return geom;
     }
 
-    this.standart_d20_dice_face_labels = [' ', '0', '1', '2', '3', '4', '5', '6', '7', '8',
-            '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'];
-    this.standart_d100_dice_face_labels = [' ', '00', '10', '20', '30', '40', '50',
-            '60', '70', '80', '90'];
+    this.standart_d20_dice_face_labels = [' ', '0', '1', '2', '3', '4', '5', '6'];
 
     this.create_dice_materials = function(face_labels, size, margin) {
         function create_text_texture(text, color, back_color) {
